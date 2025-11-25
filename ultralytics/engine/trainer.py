5851392253c514bd6152965704a7ae126fb7277d
dqh999 <dqhit999@gmail.com>
2025-11-25 15:17:04 +0700

    Fix: Prevent AssertionError when using sparsity training with AMP scaler

--- ultralytics/engine/trainer.py
+++ ultralytics/engine/trainer.py
@@ -710,11 +710,15 @@ class BaseTrainer:
 
     def optimizer_step(self):
         """Perform a single step of the training optimizer with gradient clipping and EMA update."""
-        self.scaler.unscale_(self.optimizer)  # unscale gradients
-        torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=10.0)
-        self.scaler.step(self.optimizer)
-        self.scaler.update()
-        self.optimizer.zero_grad()
+        if self.args.sr > 0.0:
+            self.optimizer.step()
+            self.optimizer.zero_grad()
+        else:
+            self.scaler.unscale_(self.optimizer)  # unscale gradients
+            torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=10.0)
+            self.scaler.step(self.optimizer)
+            self.scaler.update()
+            self.optimizer.zero_grad()
         if self.ema:
             self.ema.update(self.model)
 
