972cd8f5e80c9d69abacbf2b220333bf497bf1e7
hopquangdo <hopquangdo@gmail.com>
2025-12-19 04:20:47 +0000

    update

--- train-sparsity.py
+++ train-sparsity.py
@@ -1,15 +1,15 @@
 from ultralytics import YOLO
 
-model = YOLO("runs/train-normal/weights/best.pt")
+model = YOLO("weights/original.pt")
 
 model.train(
-    sr=1e-2,
-    lr0=1e-3,
-    data="ultralytics/cfg/datasets/coco.yaml",
-    epochs=50,
-    patience=50,
+    sr=8e-2,
+    lr0=1.3e-3,
+    data="data.yaml",
+    epochs=100,
+    patience=20,
     project='.',
     name='runs/train-sparsity',
-    batch=48,
+    batch=64,
     device=0
 )
